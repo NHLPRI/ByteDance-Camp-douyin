fee2192be42c021dba39255c5e6b5914d39d24c3
hsrain3 <947084106@qq.com>
2022-05-28 21:28:57 +0100

    add gorm model

---
+++ model/model.go
@@ -0,0 +1,43 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Name          string
+	Password      string
+	FollowCount   int64
+	FollowerCount int64
+	Videos        []Video `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+type Video struct {
+	gorm.Model
+	User           User `gorm:"ForeignKey:UserID"`
+	UserID         uint
+	PlayURL        string
+	CoverURL       string
+	FavouriteCount int64
+	CommentCount   int64
+	IsFavourite    bool
+	Title          string
+}
+
+type Follow struct {
+	ID     uint `gorm:"primary_key;AUTO_INCREMENT"`
+	UserID uint
+	FansID uint
+}
+type Like struct {
+	ID      uint `gorm:"primary_key;AUTO_INCREMENT"`
+	UserID  uint
+	VideoID uint
+}
+
+type Comment struct {
+	gorm.Model
+	UserID  uint
+	VideoID uint
+	Content string `gorm:"type:longText"`
+	Floor   uint
+}
