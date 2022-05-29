f8128180678bacedb208fefb4c7da92f846d1d93
hsrain3 <947084106@qq.com>
2022-05-29 10:35:30 +0100

    fix model type

--- model/model.go
+++ model/model.go
@@ -1,42 +1,44 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+)
 
 type User struct {
-	gorm.Model
-	Name          string
-	Password      string
-	FollowCount   int64
-	FollowerCount int64
+	ID            int64   `json:"id" gorm:"primary_key"`
+	Name          string  `json:"name" gorm:"type:varchar(32);not null;unique;index:uniqueIndex"`
+	Password      string  `json:"password" gorm:"type:varchar(32);not null"`
+	FollowCount   int64   `json:"follow_count" gorm:"not null"`
+	FollowerCount int64   `json:"follower_count" gorm:"not null"`
 	Videos        []Video `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
 type Video struct {
-	gorm.Model
-	UserID         uint
+	ID             int64 `json:"id" gorm:"primary_key"`
+	UserID         int64
 	PlayURL        string
 	CoverURL       string
 	FavouriteCount int64
 	CommentCount   int64
-	IsFavourite    bool
 	Title          string
 }
 
 type Follow struct {
-	ID     uint `gorm:"primary_key;AUTO_INCREMENT"`
-	UserID uint
-	FansID uint
+	ID     int64 `gorm:"primary_key;AUTO_INCREMENT"`
+	UserID int64
+	FansID int64
 }
 type Like struct {
-	ID      uint `gorm:"primary_key;AUTO_INCREMENT"`
-	UserID  uint
-	VideoID uint
+	ID      int64 `gorm:"primary_key;AUTO_INCREMENT"`
+	UserID  int64
+	VideoID int64
 }
 
 type Comment struct {
-	gorm.Model
-	UserID  uint
-	VideoID uint
-	Content string `gorm:"type:longText"`
-	Floor   uint
+	ID        int64 `json:"id" gorm:"primary_key"`
+	UserID    int64
+	VideoID   int64
+	Content   string `gorm:"type:longText"`
+	Floor     uint
+	CreatedAt time.Time
 }
